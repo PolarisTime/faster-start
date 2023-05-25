c066d6d79067734867290fdad08c3abf5860898c
PolarisTime <inlove@in1ove.com>
2023-05-25 22:13:51 +0800

    version 1.0.0

--- control.lua
+++ control.lua
@@ -17,32 +17,12 @@ function initPlayer(player)
 
 	local items = {
 		-- resources
-		{"coal", 150},
-		{"iron-plate", 500},
-		{"copper-plate", 300},
-		{"iron-gear-wheel", 100},
-		{"electronic-circuit", 100},
 		-- belts
-		{"fast-transport-belt", 2000},
-		{"fast-underground-belt", 200},
-		{"fast-splitter", 50},
 		-- pipes
-		{"pipe-to-ground", 50},
-		{"pipe", 50},
 		-- other logistic
-		{"fast-inserter", 500},
-		{"steel-chest", 50},
-		{"construction-robot", 10},
+		{"construction-robot", 50},
 		-- buildings
-		{"steel-furnace", 50},
-		{"assembling-machine-2", 100},
-		{"electric-mining-drill", 50},
 		-- electricity
-		{"medium-electric-pole", 200},
-		{"big-electric-pole", 50},
-		{"boiler", 10},
-		{"steam-engine", 20},
-		{"offshore-pump", 5},
 	}
 	for _, v in pairs(items) do
 		player.insert{name = v[1], count = v[2]}
@@ -53,6 +33,7 @@ function initPlayer(player)
 	local armorGrid = armorInventory.find_item_stack("power-armor").grid
 
 	local equipment = {
+		-- equipment
 		"fusion-reactor-equipment",
 		"exoskeleton-equipment",
 		"personal-roboport-equipment",
